90b9be06385ef1ea990e85b9bec415a3ea7d84f8
msbplatform <maccarter25@icloud.com>
2026-04-14 22:05:03 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import FeatureCards from "@/components/FeatureCards";
-import ImpactSection from "@/components/ImpactSection";
 import DonationTracking from "@/components/DonationTracking";
 import AnonymousDonation from "@/components/AnonymousDonation";
 import GetInvolvedSection from "@/components/GetInvolvedSection";
@@ -14,8 +13,7 @@ const Index = () => {
     <div className="min-h-screen">
       <Header />
       <HeroSection />
-      <FeatureCards />
-      <ImpactSection />
+      <FeatureCards /> 
       <DonationTracking />
       <AnonymousDonation />
       <GetInvolvedSection />
